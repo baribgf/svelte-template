8b6b22d46f9f2cc81ece1871d5f59fa0f6339708
baribgf <bougafa.005@gmail.com>
2023-06-16 22:12:41 +0100

    First commit

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<svelte:options tag="bari-app"/>
+
+<script>
+    export let name = "World";
+</script>
+
+<div id="main-container">
+    <h1>Hello, {name}!</h1>
+    <h4>Bari BGF's App</h4>
+</div>
+
+<style>
+    #main-container {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
